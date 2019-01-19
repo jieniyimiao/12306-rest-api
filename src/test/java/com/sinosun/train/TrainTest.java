f08ff49a8d5b910d9395900abb4acfee6b62c240
jieniyimiao <940433899@qq.com>
2019-01-19 22:56:49 +0800

    [Bug/Story ID]： [Bug/Story概述]： [修改描述]： 增加火车票价格分析接口 [修改人]： [审核人]：

--- src/test/java/com/sinosun/train/TrainTest.java
+++ src/test/java/com/sinosun/train/TrainTest.java
@@ -4,12 +4,15 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.google.common.base.Splitter;
+import com.google.common.collect.Maps;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.web.client.RestTemplate;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by caogu on 2018/12/24 14:23.
@@ -38,27 +41,27 @@ public class TrainTest {
     private static String endStationNo = "25"; //出发站序
     private static String seat_types = "1413"; //车票列表倒数第三个参数
 
-
-
-    private static String cLeftTicketUrl ="leftTicket/queryA";
+    private static String baseUrl = "https://kyfw.12306.cn";
+    private static String publicName = "/otn";
+    private static String leftTicketUrl ="leftTicket/queryA";
 
     //注:12306的查询接口经常改变（可能一天一改），其变动的主要规律为：https://kyfw.12306.cn/otn/leftTicket/query[A-Z]，就是最后一个字母做变动。
     // 因此，如果程序查询出异常，很大可能就是接口改变了，重新抓下查询接口即可。
-    private static String GET_TICKET_LIST_URL = "https://kyfw.12306.cn/otn/%s?" +
+    private static String getTicket_list_url = baseUrl + publicName  + "/%s?" +
             "leftTicketDTO.train_date=%s&" +
             "leftTicketDTO.from_station=%s&" +
             "leftTicketDTO.to_station=%s&" +
             "purpose_codes=%s";
 
 
-    private static String GET_TICKET_PRICE_URL = "https://kyfw.12306.cn/otn/leftTicket/queryTicketPrice?" +
+    private static String getTicketPriceUrl = baseUrl + publicName  + "/leftTicket/queryTicketPrice?" +
             "train_no=%s&" +
             "from_station_no=%s&" +
             "to_station_no=%s&" +
             "seat_types=%s&" +
             "train_date=%s";
 
-    private static String GET_TICKET_LINE_URL = "https://kyfw.12306.cn/otn/czxx/queryByTrainNo?" +
+    private static String getTicketLineUrl = baseUrl + publicName  + "/czxx/queryByTrainNo?" +
             "train_no=%s&" +
             "from_station_telecode=%s&" +
             "to_station_telecode=%s&" +
@@ -68,14 +71,14 @@ public class TrainTest {
     public void getTrainList()
     {
         // 改用jsoup请求
-        String getTicketListUrl = String.format(GET_TICKET_LIST_URL, cLeftTicketUrl, fromDate, fromStation, toStation, purposeCodes);
+        String getTicketListUrl = String.format(getTicket_list_url, leftTicketUrl, fromDate, fromStation, toStation, purposeCodes);
         JSONObject ret = restTemplate.getForObject(getTicketListUrl, JSONObject.class);
         System.out.println(JSON.toJSONString(ret, true));
 
         // 接口地址变化，获取新地址重新请求 可通过302错误码判断 302 redirect: 302 代表暂时性转移(Temporarily Moved )
         if (ret.containsKey("c_url")) {
-            cLeftTicketUrl = ret.getString("c_url");
-            getTicketListUrl = String.format(GET_TICKET_LIST_URL, cLeftTicketUrl, fromDate, fromStation, toStation, purposeCodes);
+            leftTicketUrl = ret.getString("c_url");
+            getTicketListUrl = String.format(getTicket_list_url, leftTicketUrl, fromDate, fromStation, toStation, purposeCodes);
             ret = restTemplate.getForObject(getTicketListUrl, JSONObject.class);
             System.out.println(JSON.toJSONString(ret, true));
         }
@@ -94,7 +97,7 @@ public class TrainTest {
             String trainNo = trainItem.get(2); //列车号
             String trainCode = trainItem.get(3); //车次
 
-            String startStationCode = trainItem.get(4); // 开始站代码
+            String startStationCode = trainItem.get(4); // 起始站代码
             String endStationCode = trainItem.get(5); // 结束站代码
 
             String fromStationCode = trainItem.get(6); //出发站代码
@@ -106,37 +109,36 @@ public class TrainTest {
             String arriveTime = trainItem.get(9); //到达时刻
             String runTime = trainItem.get(10); //历时
 
-            String canWebBuy = trainItem.get(11); //是否能购买：Y 可以
-            String yp_info = trainItem.get(12); //?
+            String canWebBuy = trainItem.get(11); //是否能购买：Y 可以 N 不可 IS_TIME_NOT_BUY 列车运行图调整,暂停发售
+            String ypInfo = trainItem.get(12); //?
 
-            String start_train_date = trainItem.get(13); //列车起始站发成日期?
+            String startTrainDate = trainItem.get(13); //列车起始站发成日期
 
-            String train_seat_feature = trainItem.get(14); //?
-            String location_code = trainItem.get(15); //?
-            String from_station_no = trainItem.get(16); //?
-            String to_station_no = trainItem.get(17); //?
-            String is_support_card = trainItem.get(18); //?
-            String controlled_train_flag = trainItem.get(19); //?
+            String trainSeatFeature = trainItem.get(14); //?
+            String locationCode = trainItem.get(15); //?
+            String fromStationNo = trainItem.get(16); //出发站站序（对应火车经停信息中的站序）01表示始发站，大于1则表示过站
+            String toStationNo = trainItem.get(17); //到达站站序（对应火车经停信息中的站序）
+            String isSupportCard = trainItem.get(18); //可凭二代身份证直接进出站 1 可以 0 不可以
+            String controlledTrainFlag = trainItem.get(19); //?
 
-            String gg_num = StringUtils.isNotEmpty(trainItem.get(20)) ? trainItem.get(20) : "--"; //?
-            String gr_num = StringUtils.isNotEmpty(trainItem.get(21)) ? trainItem.get(21) : "--"; // 高级软卧
-            String qt_num = StringUtils.isNotEmpty(trainItem.get(22)) ? trainItem.get(22) : "--"; // 其他
-            String rw_num = StringUtils.isNotEmpty(trainItem.get(23)) ? trainItem.get(23) : "--"; // 软卧
-            String rz_num = StringUtils.isNotEmpty(trainItem.get(24)) ? trainItem.get(24) : "--"; // 软座
-            String tz_num = StringUtils.isNotEmpty(trainItem.get(25)) ? trainItem.get(25) : "--"; //?
-            String wz_num = StringUtils.isNotEmpty(trainItem.get(26)) ? trainItem.get(26) : "--"; // 无座
-            String yb_num = StringUtils.isNotEmpty(trainItem.get(27)) ? trainItem.get(27) : "--"; //?
-            String yw_num = StringUtils.isNotEmpty(trainItem.get(28)) ? trainItem.get(28) : "--"; // 硬卧
-            String yz_num = StringUtils.isNotEmpty(trainItem.get(29)) ? trainItem.get(29) : "--"; // 硬座
-            String ze_num = StringUtils.isNotEmpty(trainItem.get(30)) ? trainItem.get(30) : "--"; // 二等座
-            String zy_num = StringUtils.isNotEmpty(trainItem.get(31)) ? trainItem.get(31) : "--"; // 一等座
-            String swz_num = StringUtils.isNotEmpty(trainItem.get(32)) ? trainItem.get(32) : "--"; // 商务特等座
-            String srrb_num = StringUtils.isNotEmpty(trainItem.get(33)) ? trainItem.get(33) : "--"; //?
+            String ggNum = StringUtils.isNotEmpty(trainItem.get(20)) ? trainItem.get(20) : "--"; //?
+            String grNum = StringUtils.isNotEmpty(trainItem.get(21)) ? trainItem.get(21) : "--"; // 高级软卧
+            String qtNum = StringUtils.isNotEmpty(trainItem.get(22)) ? trainItem.get(22) : "--"; // 其他
+            String rwNum = StringUtils.isNotEmpty(trainItem.get(23)) ? trainItem.get(23) : "--"; // 软卧，一等卧
+            String rzNum = StringUtils.isNotEmpty(trainItem.get(24)) ? trainItem.get(24) : "--"; // 软座
+            String tzNum = StringUtils.isNotEmpty(trainItem.get(25)) ? trainItem.get(25) : "--"; //? 特等座?
+            String wzNum = StringUtils.isNotEmpty(trainItem.get(26)) ? trainItem.get(26) : "--"; // 无座
+            String ybNum = StringUtils.isNotEmpty(trainItem.get(27)) ? trainItem.get(27) : "--"; //?
+            String ywNum = StringUtils.isNotEmpty(trainItem.get(28)) ? trainItem.get(28) : "--"; // 硬卧，二等卧
+            String yzNum = StringUtils.isNotEmpty(trainItem.get(29)) ? trainItem.get(29) : "--"; // 硬座
+            String edzNum = StringUtils.isNotEmpty(trainItem.get(30)) ? trainItem.get(30) : "--"; // 二等座
+            String ydzNum = StringUtils.isNotEmpty(trainItem.get(31)) ? trainItem.get(31) : "--"; // 一等座
+            String swzNum = StringUtils.isNotEmpty(trainItem.get(32)) ? trainItem.get(32) : "--"; // 商务座特等座
+            String srrbNum = StringUtils.isNotEmpty(trainItem.get(33)) ? trainItem.get(33) : "--"; // 动卧
 
             String yp_ex = trainItem.get(34); //? 查询车票价格时的seat_types字段
-            String seat_types = trainItem.get(35); //?
-            String exchange_train_flag = trainItem.get(36); //?
-
+            String seatTypes = trainItem.get(35); //?
+            String exchangeTrainFlag = trainItem.get(36); //?
 
             System.out.println("列车号:" + trainCode);
             System.out.println("出发站:" + fromStationName);
@@ -144,19 +146,19 @@ public class TrainTest {
             System.out.println("出发时间:" + startTime);
             System.out.println("到达时间:" + arriveTime);
             System.out.println("历时:" + runTime);
-            System.out.println("列车起始站发车日期:" + start_train_date);
+            System.out.println("列车起始站发车日期:" + startTrainDate);
 
-            System.out.println("商务座特等座:" + swz_num);
-            System.out.println("一等座:" + zy_num);
-            System.out.println("二等座:" + ze_num);
-            System.out.println("高级软卧:" + gr_num);
-            System.out.println("软卧:" + rw_num);
+            System.out.println("商务座特等座:" + swzNum);
+            System.out.println("一等座:" + ydzNum);
+            System.out.println("二等座:" + edzNum);
+            System.out.println("高级软卧:" + grNum);
+            System.out.println("软卧:" + rwNum);
             System.out.println("动卧:" + "+++");
-            System.out.println("硬卧:" + yw_num);
-            System.out.println("软座:" + rz_num);
-            System.out.println("硬座:" + yz_num);
-            System.out.println("无座:" + wz_num);
-            System.out.println("其他:" + qt_num);
+            System.out.println("硬卧:" + ywNum);
+            System.out.println("软座:" + rzNum);
+            System.out.println("硬座:" + yzNum);
+            System.out.println("无座:" + wzNum);
+            System.out.println("其他:" + qtNum);
             System.out.println("备注:" + buttonTextInfo);
             System.out.println("\n");
 
@@ -169,18 +171,19 @@ public class TrainTest {
     @Test
     public void queryByTrainNo() {
         // 改用jsoup请求
-        String getTicketListUrl = String.format(GET_TICKET_LINE_URL, trainNo, fromStation, toStation, startTrainDate);
+        String getTicketListUrl = String.format(getTicketLineUrl, trainNo, fromStation, toStation, startTrainDate);
         JSONObject ret = restTemplate.getForObject(getTicketListUrl, JSONObject.class);
         System.out.println(JSON.toJSONString(ret, true));
 
         JSONObject result = new JSONObject();
 
         JSONArray stops = ret.getJSONObject("data").getJSONArray("data");
+
         JSONObject stopInfoFirst = stops.getJSONObject(0);
         String startStationName = stopInfoFirst.getString("start_station_name"); //出发城市
         String endStationName = stopInfoFirst.getString("end_station_name"); //到达城市
         String stationTrainCode = stopInfoFirst.getString("station_train_code"); //车次号
-        String trainClassName = stopInfoFirst.getString("train_class_name"); //车次类型
+        String trainClassName = stopInfoFirst.getString("train_class_name"); //车次类型， 快速等
         String serviceType = stopInfoFirst.getString("service_type"); //服务类型 0表示无空调 其他表示有空调
         String serviceName = "0".equals(serviceType) ? "无空调" : "有空调";
 
@@ -195,11 +198,11 @@ public class TrainTest {
         for (int i = 0; i < stops.size(); i++) {
             JSONObject stopInfo = stops.getJSONObject(i);
             String startTime = stopInfo.getString("start_time"); //出发时间（格式 HH:mm）
-            String arriveTime = stopInfo.getString("arrive_time"); //到达时间（格式 HH:mm）
+            String arriveTime = stopInfo.getString("arrive_time"); //到达时间（格式 HH:mm 或者----）
             String stationName = stopInfo.getString("station_name"); //站名
-            String stopoverTime = stopInfo.getString("stopover_time"); //停留时间（分钟）
-            String stationNo = stopInfo.getString("station_no"); //站序
-            Boolean isEnabled = stopInfo.getBoolean("isEnabled"); //是否有效
+            String stopoverTime = stopInfo.getString("stopover_time"); //停留时间（分钟） 可能为----
+            String stationNo = stopInfo.getString("station_no"); //站序（01开始）
+            Boolean isEnabled = stopInfo.getBoolean("isEnabled"); //是否是我们搜索的出行站和到达站 false不是 true是
 
             JSONObject resultStopInfo = new JSONObject();
             resultStopInfo.put("StartTime", startTime);
@@ -219,16 +222,65 @@ public class TrainTest {
     @Test
     public void queryTicketPrice() {
         // 改用jsoup请求
-        String getTicketListUrl = String.format(GET_TICKET_PRICE_URL, trainNo, startStationNo, endStationNo, seat_types, fromDate);
+        String getTicketListUrl = String.format(getTicketPriceUrl, trainNo, startStationNo, endStationNo, seat_types, fromDate);
         JSONObject ret = restTemplate.getForObject(getTicketListUrl, JSONObject.class);
         System.out.println(JSON.toJSONString(ret, true));
 
         JSONObject data = ret.getJSONObject("data");
 
+        // A9  商务座/特等座
+        // M   一等座
+        // O   二等座
+        // A6  高级软卧
+        // A4  软卧/一等卧
+        // F   动卧
+        // A3  硬卧/二等卧
+        // A2  软座 (广州到汉口)
+        // A1  硬座
+        // WZ  无座
+        Map<String, String> setType = Maps.newHashMap();
+        setType.put("swz", "A9");
+        setType.put("ydz", "M");
+        setType.put("edz", "O");
+        setType.put("gr", "A6");
+        setType.put("rw", "A4");
+        setType.put("dw", "F");
+        setType.put("yw", "A3");
+        setType.put("rz", "A2");
+        setType.put("yz", "A1");
+        setType.put("wz", "WZ");
+
+        BigDecimal swzPrice = processPrice(data.getString(setType.get("swz")));
+        BigDecimal ydzPrice = processPrice(data.getString(setType.get("ydz")));
+        BigDecimal edzPrice = processPrice(data.getString(setType.get("edz")));
+        BigDecimal grPrice = processPrice(data.getString(setType.get("gr")));
+        BigDecimal rwPrice = processPrice(data.getString(setType.get("rw")));
+        BigDecimal dwPrice = processPrice(data.getString(setType.get("dw")));
+        BigDecimal ywPrice = processPrice(data.getString(setType.get("yw")));
+        BigDecimal rzPrice = processPrice(data.getString(setType.get("rz")));
+        BigDecimal yzPrice = processPrice(data.getString(setType.get("yz")));
+        BigDecimal wzPrice = processPrice(data.getString(setType.get("wz")));
+
+        System.out.println(swzPrice);
+        System.out.println(ydzPrice);
+        System.out.println(edzPrice);
+        System.out.println(grPrice);
+        System.out.println(grPrice);
+        System.out.println(rwPrice);
+        System.out.println(dwPrice);
+        System.out.println(ywPrice);
+        System.out.println(rzPrice);
+        System.out.println(yzPrice);
+        System.out.println(wzPrice);
 
     }
 
-
-
+    private BigDecimal processPrice(String price) {
+        BigDecimal ret = null;
+        if (StringUtils.isNotEmpty(price) && price.startsWith("¥")) {
+            ret = new BigDecimal(price.substring(1, price.length()));
+        }
+        return ret;
+    }
 
 }
